78688c663db95b8e841cae6f19925a749b8a1a5a
przybysz.tomasz <przybysz.tomasz@gmail.com>
2013-06-01 12:01:11 +0000

    add: heating_controller

---
+++ prg/STM32/src/heating_controller/src/main.c
@@ -0,0 +1,214 @@
+//============================================================================
+// Name        : STM32_Dev_Template.cpp
+// Author      : 
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "stm32f10x.h"
+
+
+#include "FreeRTOS.h"
+#include "task.h"
+#include "queue.h"
+
+#include "FreeRTOSConfig.h"
+
+#include "serial_port.h"
+
+
+#define mainECHO_TASK_PRIORITY				( tskIDLE_PRIORITY + 1 )
+
+//using namespace std;
+
+//------------------------------------------------------------------------------
+//------------------------------------------------------------------------------
+
+static void prvSetupHardware(void);
+
+void GPIO_Configuration(void);
+void NVIC_Configuration(void);
+void USART_Configuration(void);
+
+
+static void prvUSARTEchoTask(void *pvParameters);
+
+
+#define mainBLOCK_Q_PRIORITY				( tskIDLE_PRIORITY + 2 )
+
+
+
+//------------------------------------------------------------------------------
+//------------------------------------------------------------------------------
+
+void  Delay (uint32_t nCount)
+{
+  for(; nCount != 0; nCount--);
+}
+
+
+//------------------------------------------------------------------------------
+//------------------------------------------------------------------------------
+
+int main()
+	{
+
+	USART_InitTypeDef USART_InitStructure;
+
+
+	GPIO_Configuration();
+	NVIC_Configuration();
+
+	prvSetupHardware();
+
+
+
+	USART_InitStructure.USART_BaudRate = 57600;
+	USART_InitStructure.USART_WordLength = USART_WordLength_8b;
+	USART_InitStructure.USART_StopBits = USART_StopBits_1;
+	USART_InitStructure.USART_Parity = USART_Parity_No;
+
+	serial_port_init(1, &USART_InitStructure); // console
+	serial_port_rx_timeout_set(1, 5000);
+
+
+	xTaskCreate( prvUSARTEchoTask, ( signed char * ) "Echo", configMINIMAL_STACK_SIZE, NULL, mainECHO_TASK_PRIORITY, NULL);
+
+
+	vTaskStartScheduler();
+
+	return 0;
+	}
+
+//------------------------------------------------------------------------------
+//------------------------------------------------------------------------------
+
+void GPIO_Configuration(void)
+	{
+	GPIO_InitTypeDef GPIO_InitStructure;
+
+	RCC_APB2PeriphClockCmd( RCC_APB2Periph_GPIOB | RCC_APB2Periph_GPIOC , ENABLE);
+
+	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_0 | GPIO_Pin_1;
+	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
+	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_Out_PP;
+	GPIO_Init(GPIOB, &GPIO_InitStructure);
+
+	}
+
+//------------------------------------------------------------------------------
+
+void NVIC_Configuration(void)
+	{
+	NVIC_InitTypeDef NVIC_InitStructure;
+
+	NVIC_PriorityGroupConfig(NVIC_PriorityGroup_1);
+
+	// USART1 DMA Interrupt
+	NVIC_InitStructure.NVIC_IRQChannel= DMA1_Channel4_IRQn;
+	NVIC_InitStructure.NVIC_IRQChannelPreemptionPriority= 0;
+	NVIC_InitStructure.NVIC_IRQChannelSubPriority= 0;
+	NVIC_InitStructure.NVIC_IRQChannelCmd= ENABLE;
+	NVIC_Init(&NVIC_InitStructure);
+
+	// USART1 Interrupt
+	NVIC_InitStructure.NVIC_IRQChannel= USART1_IRQn;
+	NVIC_InitStructure.NVIC_IRQChannelPreemptionPriority= 0;
+	NVIC_InitStructure.NVIC_IRQChannelSubPriority= 0;
+	NVIC_InitStructure.NVIC_IRQChannelCmd= ENABLE;
+	NVIC_Init(&NVIC_InitStructure);
+
+
+
+	}
+
+
+//------------------------------------------------------------------------------
+
+static void prvUSARTEchoTask(void *pvParameters)
+	{
+	int result;
+
+	//char *text= "test";
+	char rxbuff[32];
+	char txbuff[32];
+
+
+	while (1)
+		{
+
+		result= serial_port_read(1, rxbuff, 32);
+
+		switch (result)
+			{
+
+			case -1:
+				{
+				printf("recv: error\n");
+				break;
+				}
+
+			case 0:
+				{
+				printf("recv: timeout\n");
+
+				break;
+				}
+
+			default:
+				{
+				int x;
+				printf("recv[%02d]:", result);
+
+				for (x=0;x<result;x++)
+					printf(" %02X", rxbuff[x]);
+
+				printf("\n");
+
+				break;
+				}
+
+			}
+
+//		GPIO_SetBits(GPIOB , GPIO_Pin_0 | GPIO_Pin_1);
+//		GPIO_ResetBits(GPIOB , GPIO_Pin_0 | GPIO_Pin_1);
+
+	//	vTaskDelay(1000);
+
+	    }
+
+
+	}
+
+//------------------------------------------------------------------------------
+
+static void prvSetupHardware(void)
+	{
+	/* Configure HCLK clock as SysTick clock source. */
+	//SysTick_CLKSourceConfig(SysTick_CLKSource_HCLK);
+	}
+
+//------------------------------------------------------------------------------
+
+void vApplicationStackOverflowHook( xTaskHandle pxTask, signed char *pcTaskName )
+	{
+	/* This function will get called if a task overflows its stack.   If the
+	parameters are corrupt then inspect pxCurrentTCB to find which was the
+	offending task. */
+
+	GPIO_ResetBits(GPIOB , GPIO_Pin_0 | GPIO_Pin_1);
+
+	( void ) pxTask;
+	( void ) pcTaskName;
+
+	for( ;; );
+	}
+
+//------------------------------------------------------------------------------
+
