390c5e0010189343bfb067c8aa0fa9b66bdd7295
przybysz.tomasz <przybysz.tomasz@gmail.com>
2014-02-01 11:47:15 +0000

    1wire: add: wyszukiwanie termometrów na magistrali

--- prg/STM32/src/lib/1wire.c
+++ prg/STM32/src/lib/1wire.c
@@ -57,6 +57,9 @@ ktimerlst_spec_s onewire_timer;
 k_uchar callback_phase;
 xQueueHandle owire_queue= NULL;
 
+int LastDiscrepancy;
+int LastFamilyDiscrepancy;
+int LastDeviceFlag;
 
 void onewire_write_bit_pullup(onewire_handler_s *onewire_handler, k_uchar bit);
 
@@ -307,6 +310,169 @@ void owire_reset_callback(ktimerlst_spec_s *ktimerlst_spec)
 
 		} // switch (ktimerlst_spec->phase)
 
+	}
+
+//------------------------------------------------------------------------------
+//------------------------------------------------------------------------------
+
+k_uchar onewire_dev_search(onewire_handler_s *onewire_handler, k_uchar *dev_id)
+	{
+	int id_bit_number;
+	int last_zero, rom_byte_number, search_result;
+	int id_bit, cmp_id_bit;
+	unsigned char rom_byte_mask, search_direction;
+	k_uchar crc8;
+
+	// initialize for search
+	id_bit_number = 1;
+	last_zero = 0;
+	rom_byte_number = 0;
+	rom_byte_mask = 1;
+	search_result = 0;
+	crc8 = 0;
+
+	memset(dev_id, 0, 8);
+
+	// if the last call was not the last one
+	if (!LastDeviceFlag)
+		{
+
+		// 1-Wire reset
+		if (onewire_bus_reset(onewire_handler) != 0x00)
+			{
+			// reset the search
+			LastDiscrepancy = 0;
+			LastDeviceFlag = FALSE;
+			LastFamilyDiscrepancy = 0;
+			return FALSE;
+			}
+
+		// issue the search command
+		onewire_write_byte(onewire_handler, ONE_WIRE_CMD_SEARCH_ROM);
+
+		  // loop to do the search
+		do
+			{
+			// read a bit and its complement
+			id_bit = onewire_read_bit(onewire_handler);
+			cmp_id_bit = onewire_read_bit(onewire_handler);
+
+			// check for no devices on 1-wire
+			if ((id_bit == 1) && (cmp_id_bit == 1))
+				break;
+			else
+				{
+				// all devices coupled have 0 or 1
+				if (id_bit != cmp_id_bit)
+					search_direction = id_bit;  // bit write value for search
+				else
+					{
+					// if this discrepancy if before the Last Discrepancy
+					// on a previous next then pick the same as last time
+					if (id_bit_number < LastDiscrepancy)
+						search_direction = ((dev_id[rom_byte_number] & rom_byte_mask) > 0);
+					else
+					// if equal to last pick 1, if not then pick 0
+						search_direction = (id_bit_number == LastDiscrepancy);
+
+					// if 0 was picked then record its position in LastZero
+					if (search_direction == 0)
+						{
+						last_zero = id_bit_number;
+
+						// check for Last discrepancy in family
+						if (last_zero < 9)
+						LastFamilyDiscrepancy = last_zero;
+						}
+					}
+
+				// set or clear the bit in the ROM byte rom_byte_number
+				// with mask rom_byte_mask
+				if (search_direction == 1)
+					dev_id[rom_byte_number] |= rom_byte_mask;
+				else
+					dev_id[rom_byte_number] &= ~rom_byte_mask;
+
+				// serial number search direction write bit
+				onewire_write_bit(onewire_handler, search_direction);
+
+				// increment the byte counter id_bit_number
+				// and shift the mask rom_byte_mask
+				id_bit_number++;
+				rom_byte_mask <<= 1;
+
+				// if the mask is 0 then go to new SerialNum byte rom_byte_number and reset mask
+				if (rom_byte_mask == 0)
+					{
+					onewire_crc8(&crc8, dev_id[rom_byte_number]); // accumulate the CRC
+					rom_byte_number++;
+					rom_byte_mask = 1;
+					}
+				}
+
+			} while(rom_byte_number < 8);  // loop until through all ROM bytes 0-7
+
+			// if the search was successful then
+			if (!((id_bit_number < 65) || (crc8 != 0)))
+				{
+				// search successful so set LastDiscrepancy,LastDeviceFlag,search_result
+				LastDiscrepancy = last_zero;
+
+				// check for last device
+				if (LastDiscrepancy == 0)
+					LastDeviceFlag = TRUE;
+
+				search_result = TRUE;
+				}
+		}
+
+
+	// if no device found then reset counters so next 'search' will be like a first
+	if (!search_result || !dev_id[0])
+		{
+		LastDiscrepancy = 0;
+		LastDeviceFlag = FALSE;
+		LastFamilyDiscrepancy = 0;
+		search_result = FALSE;
+		}
+
+	return search_result;
+	}
+
+//------------------------------------------------------------------------------
+
+k_uchar onewire_dev_search_first(onewire_handler_s *onewire_handler, k_uchar *dev_id, k_uchar dev_family)
+	{
+	if (!onewire_handler || !dev_id)
+		return 0x00;
+
+	// reset the search state
+
+	if (dev_family == 0x00)
+		{
+		LastDiscrepancy = 0;
+		LastDeviceFlag = FALSE;
+		LastFamilyDiscrepancy = 0;
+		}
+	else
+		{
+		dev_id[0]= dev_family;
+		LastDiscrepancy = 64;
+		LastDeviceFlag = FALSE;
+		LastFamilyDiscrepancy = 0;
+		}
+
+	return onewire_dev_search(onewire_handler, dev_id);
+	}
+
+//------------------------------------------------------------------------------
+
+k_uchar onewire_dev_search_next(onewire_handler_s *onewire_handler, k_uchar *dev_id)
+	{
+	if (!onewire_handler || !dev_id)
+		return 0x00;
+
+	return onewire_dev_search(onewire_handler, dev_id);
 	}
 
 //------------------------------------------------------------------------------
